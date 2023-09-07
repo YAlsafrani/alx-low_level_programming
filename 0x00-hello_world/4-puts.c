27ca97a6d808c0b1c1df9a97981e9ca3a34b7f6c
Rimuru Tempest <rimurutemest1589@gmail.com>
2023-09-07 08:35:49 -0700

    updated 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main()
+/*function description
+ * print a statement using puts function and retuns 0
+*/
+
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
