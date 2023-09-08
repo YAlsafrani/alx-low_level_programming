7f5764e069ed3af44430b8a803b0da1df948c91a
Rimuru Tempest <rimurutemest1589@gmail.com>
2023-09-08 08:13:05 -0700

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,14 @@
  */
 int main(void)
 {
-    char ch;
-
-    for (ch = 'a'; ch <= 'z'; ch++)
-    {
-        putchar(ch);
+	char ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	
 	putchar('\n');
-    }
-
-    return (0);
+	
+	return (0);
 }
