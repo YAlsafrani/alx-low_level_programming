8d994ba7d5ba912d4cd204609773b33148ea6629
Rimuru Tempest <rimurutemest1589@gmail.com>
2023-09-12 11:06:26 -0700

    added 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - Returns if char is lower case 
+ *
+ * Return: 1 for lower case, 0 otherwise
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
