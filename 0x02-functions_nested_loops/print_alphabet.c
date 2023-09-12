1e014c05d25319773e481591e3f3e05b2ad5a1b6
Rimuru Tempest <rimurutemest1589@gmail.com>
2023-09-12 09:51:01 -0700

    added 1

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return: void
+*/
+
+void print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
