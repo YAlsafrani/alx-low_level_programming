dbaa327599e3c650d9c07e92895ca8ba5d2501c1
Rimuru Tempest <rimurutemest1589@gmail.com>
2023-09-14 10:37:28 -0700

    update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,11 @@
 
 void print_most_numbers(void)
 {
-	char digit;
+	int digit;
 
-	for (digit = '0'; digit <= '9'; digit++)
+	for (digit = 0; digit <= 9; digit++)
 	{
-		if (digit == '2' || digit == '4')
+		if (digit == 2 || digit == 4)
 		{
 			continue;
 		}
