37960c06ffbf39e0e43881d09c5c1e3c76fa7eb1
Rimuru Tempest <rimurutemest1589@gmail.com>
2023-09-14 10:58:11 -0700

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,9 +15,9 @@ void more_numbers(void)
 		{
 			if (num > 9)
 			{
-				_putchar('0' + (num / 10)); // Print the tens digit
+				_putchar('0' + (num / 10));
 			}
-			_putchar('0' + (num % 10)); // Print the ones digit
+			_putchar('0' + (num % 10));
 		}
 
 		_putchar('\n');
