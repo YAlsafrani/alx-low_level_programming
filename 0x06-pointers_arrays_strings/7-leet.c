d373cb214198fb848e468afa02b88bb7ee789bad
Rimuru Tempest <rimurutemest1589@gmail.com>
2023-09-21 02:47:00 -0700

    update

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,13 +12,13 @@ char *leet(char *str)
 	char leet_replacements[] = "4433007711";
 	int i, j;
 
-	for (i = 0; n[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (n[i] == leet_table[j])
+			if (str[i] == leet_table[j])
 			{
-				n[i] = leet_replacements[j];
+				str[i] = leet_replacements[j];
 			}
 		}
 	}
