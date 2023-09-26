275fe400d2f892eb7ff1f46717dc0df555ee46fe
Rimuru Tempest <rimurutemest1589@gmail.com>
2023-09-25 18:48:44 -0700

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,7 +6,7 @@
  * @accept: Pointer to the accept string.
  *
  * Return: Number of bytes in the initial segment of s which consist only
- *         of bytes from accept.
+ * of bytes from accept.
  */
 unsigned int _strspn(char *s, char *accept)
 {
