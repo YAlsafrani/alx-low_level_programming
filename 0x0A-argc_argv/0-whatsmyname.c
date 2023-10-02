873933f0c542e82878986d8ba87ffe6522a881d7
Rimuru Tempest <rimurutemest1589@gmail.com>
2023-10-02 07:24:35 -0700

    added 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * @argc: argument.
+ * @argv: argument.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
