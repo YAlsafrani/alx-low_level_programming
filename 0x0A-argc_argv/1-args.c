32f5a26374fdb842a46066a23e591b60a5d02b5a
Rimuru Tempest <rimurutemest1589@gmail.com>
2023-10-02 07:27:03 -0700

    added 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * @argc: argument.
+ * @argv: argument.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int num_args = argc - 1;
+
+	printf("%d\n", num_args);
+
+	return (0);
+}
